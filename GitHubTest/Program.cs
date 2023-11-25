98f813e69e04dc6ff9ae88a6e53b35295789480d
R2NBT <R2NBT@R2NB4>
2023-11-25 14:26:31 +0200

    Добавьте файлы проекта.

---
+++ GitHubTest/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GitHubTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int[,] data = new int[5,5];
+
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    data[i, j] = rnd.Next(0, 100);
+                }
+            }
+
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    Console.Write(data[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
